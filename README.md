3b8b6d11554615c1ad9d860aa12a05a9df8e5602
AnJaeSeong <dktmzh6@gmail.com>
2022-10-18 22:42:20 +0900

    test: camera work, shader

--- README.md
+++ README.md
@@ -2,46 +2,137 @@
 개인 블로그를 직접 만들어 봅니다. react, three.js 활용을 목표로 합니다.
 
 ## three.js 기록
+이 문서 내용은 Pmndrs.docs를 기반으로 작성되었습니다
+
+https://docs.pmnd.rs/
+
 - react에서는 react-three/fiber를 사용해야함
 - react-three/fiber와 react-three/drei를 활용
 
+#### useFrame
+- fiber hook 의 하나로 fiber render loop의 모든 프레임 상에서 코드를 실행한다
+- fiber hook은 반드시 <Canvas /> 내에서만 호출 할 수 있다.
+
+- 보통 상태 변경시 setState 방식을 생각할 수 있지만 state를 통한 mesh의 update는 이상적이지 않다
+- useRef로 mesh에 접근하여 값을 직접 바꿔주는게 효율적
+
+- 각 mesh에 이벤트 핸들러를 설정할 수 있음 -> https://docs.pmnd.rs/react-three-fiber/tutorials/events-and-interaction
+
+-
+
+##### react spring
+- mesh의 움직임을 좀더 부드럽게 만들어보자
+
+##### drei
+- fiber 라이브러리를 좀더 풍부하게 (셰이더, 카메라 등)
+
+
+
+
+#### useFrame의 효율적인 작업을 위한 코드 작성법
+
 ##### useFrame에서 각 프레임 별 애니메이션 설정 (unity frame과 유사한 개념인듯하다. 자세한 내용은 조사 필요)
+- useEffect(() => setInterval(() => ())) 와 유사한 동작 매커니즘을 보이는듯 함
+
 ##### 컴포넌트의 prop에 직접 접근하는 것 보다 useRef로 접근하여 값을 조작하는 것이 효율이 더 좋다고 함 
 
+``` C
 const ref = useRef()
 useFrame((state, delta) => (ref.current.position.x += delta))
 return <mesh ref={ref} />
+```
 
 ##### useFrame + lerp 조합으로 부드러운 애니메이션 연출이 가능하다
 - lerp: linear interpolation 선형 보간법, 애니메이션에 가속도 효과를 주는 법
 
+``` C
 function Signal({ active }) {
   const ref = useRef()
   useFrame((state, delta) => {
     ref.current.position.x = THREE.MathUtils.lerp(ref.current.position.x, active ? 100 : 0, 0.1)
   })
   return <mesh ref={ref} />
+```
 
 ##### redux useSelector로 상태값 조회를 애니메이션 컴포넌트 or props에 직접 접근하지 않도록 해야한다
-
+ 
+``` C
 const x = useSelector((state) => state.x)
 return <mesh position-x={x} />
+```
 -이렇게 접근할 경우 60fps 의 프레임마다 조회 수행
 
+``` C
 useFrame(() => (ref.current.position.x = api.getState().x))
 return <mesh ref={ref} />
+```
 -getState 메서드를 활용하자
 
 ##### 무차별적인 런타임 마운트를 자제하자
 - threejs는 re-mount를 하지 않는것이 일반적인 방법이다. Re-mount를 할 경우 buffer와 materials가 재초기화/컴파일 을 수행하는데 이는 매우 비효율적인 작업
 
+``` C
 {
   stage === 1 && <Stage1 />
 }
 {
   stage === 2 && <Stage2 />
 }
+```
+이는 런타임 상황에서 리렌더링을 수행하는 코드
+아래처럼 바꾸자 (visible속성을 활용하여 숨기는 방식)
 
+``` C
+<Stage1 visible={stage === 1} />
+<Stage2 visible={stage === 2} />
+<Stage3 visible={stage === 3} />
 
+function Stage1(props) {
+  return (
+    <group {...props}>
+    ...
+```
 
-- useEffect(() => setInterval(() => ())) 와 유사한 동작 매커니즘을 보이는듯 함
+##### loop 내에서 새 객체를 생성하지 말것
+``` C
+useFrame(() => {
+  ref.current.position.lerp(new THREE.Vector3(x, y, z), 0.1)
+})
+```
+new 키워드로 Vector3객체를 새로이 생성하고 있다 이는 프레임마다 새 객체를 생성하는 비효율적인 코드
+아래와 같이 수정하자
+
+``` C
+function Foo({ vec = new THREE.Vector(), ...props })
+  useFrame(() => {
+    ref.current.position.lerp(vec.set(x, y, z), 0.1)
+  })
+```
+
+##### 일반로더 말고 useLoader를 사용하자
+- threejs 로더는 비동기 자원을 로드할 수 있는 기능을 제공하나 재사용 하는 방식을 반드시 사용할것, 사용시마다 로드를 한다면 비효율적이다
+``` C
+function Component() {
+  const [texture, set] = useState()
+  useEffect(() => void new TextureLoader().load(url, set), [])
+  return texture ? (
+    <mesh>
+      <sphereGeometry />
+      <meshBasicMaterial map={texture} />
+    </mesh>
+  ) : null
+}
+```
+texture 리소스를 마운트마다(useEffect) 리로드 하고 있다. 아래와 같이 바꾸자
+
+```C
+function Component() {
+  const texture = useLoader(TextureLoader, url)
+  return (
+    <mesh>
+      <sphereGeometry />
+      <meshBasicMaterial map={texture} />
+    </mesh>
+  )
+}
+```
--- src/CanvasTest.js
+++ src/CanvasTest.js
@@ -1,61 +1,105 @@
-import { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState, useMemo } from "react";
+import { Canvas, useFrame, useThree} from "@react-three/fiber";
+import { MeshReflectorMaterial, distortionTexture } from "@react-three/drei";
 import styled from "styled-components";
-import { OrthographicCamera, OrbitControls } from "@react-three/drei";
 import * as THREE from 'three';
-import { AxesHelper } from "three";
+import { useSpring, animated } from "@react-spring/three";
+
+// CameraControls.install({ THREE })
 
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
 `;
 
-function Box(props) {
-    const ref = useRef()
+function Box({position}) {
+    const ref = useRef();
     // Hold state for hovered and clicked events
     const [hovered, hover] = useState(false)
     const [clicked, click] = useState(false)
+
+    const { scale } = useSpring({ scale: clicked ? 1.5 : 1})
     // Subscribe this component to the render-loop, rotate the mesh every frame
+    let vec = new THREE.Vector3()
     useFrame((state, delta) => {
-      ref.current.rotation.x += 0.01
-      if (clicked){
-        state.camera.position.lerp(new THREE.Vector3().set(ref.current.position.x, 0, 3), .005)
-        // state.camera.quaternion.slerp(
-        //   new THREE.Quaternion.setFromAxisAngle(new THREE.Vector3( 0, 1, 0 ), Math.PI / 2), .005)
-      } else {
-        state.camera.position.lerp(new THREE.Vector3().set(0, 0, 10), .005)
+      ref.current.rotation.x += 0.01      
+      // console.log(state.camera.position);
+      if(clicked) {
+        state.camera.position.lerp(vec.set(ref.current.position.x*10, 0, 3), .01)
+        state.camera.lookAt(ref.current.position)
+      }else {
+        state.camera.position.lerp(vec.set(0,10,10), .01)
+        state.camera.lookAt(vec.set(0,0,0))
+        state.camera.up.set(0, 0, 1)
       }
       
     })
+    
     // Return the view, these are regular Threejs elements expressed in JSX
     return (
-      <mesh
-        {...props}
+      <animated.mesh
+        position={position}
         ref={ref}
-        scale={clicked ? 1.5 : 1}
-        onClick={(event) => click(!clicked)}
+        scale={scale}
+        onClick={(e) => {
+          click(!clicked);
+          // zoomToView(e.object.position);
+        }}
         onPointerOver={(event) => hover(true)}
         onPointerOut={(event) => hover(false)}>
         <boxGeometry args={[1, 2, 2]} />
         <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
+      </animated.mesh>
     )
 }
 
+function Plane() {
+  return (
+    <mesh position={[0, 0, -5]}>
+      <planeGeometry args={[100, 100]}/>
+      <MeshReflectorMaterial
+        color="#151515"
+        blur={[1000, 1000]} // Blur ground reflections (width, heigt), 0 skips blur
+        mixBlur={1} // How much blur mixes with surface roughness (default = 1)
+        mixStrength={1} // Strength of the reflections
+        mixContrast={1} // Contrast of the reflections
+        resolution={256} // Off-buffer resolution, lower=faster, higher=better quality, slower
+        mirror={0} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+        depthScale={1} // Scale the depth factor (0 = no depth, default = 0)
+        minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+        maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+        depthToBlurRatioBias={0.25} // Adds a bias factor to the depthTexture before calculating the blur amount [blurFactor = blurTexture * (depthTexture + bias)]. It accepts values between 0 and 1, default is 0.25. An amount > 0 of bias makes sure that the blurTexture is not too sharp because of the multiplication with the depthTexture
+        distortion={1} // Amount of distortion based on the distortionMap texture
+        distortionMap={null} // The red channel of this texture is used as the distortion map. Default is null
+        debug={0} /* Depending on the assigned value, one of the following channels is shown:
+          0 = no debug
+          1 = depth channel
+          2 = base channel
+          3 = distortion channel
+          4 = lod channel (based on the roughness)
+        */
+      reflectorOffset={0.2} // Offsets the virtual camera that projects the reflection. Useful when the reflective surface is some distance from the object's origin (default = 0)
+      />
+    </mesh>
+  );
+}
+
 function CanvasTest() {
-    return(
-        <Wrapper>
-          <Canvas>
-              <primitive object={new THREE.AxesHelper(10)} />
-              <ambientLight />
-              {/* <pointLight position={[10, 10, 10]} /> */}
-              <directionalLight color="white" position={[10, 10, 10]} />
-              <Box position={[-1.2, 0, 0]} />
-              <Box position={[1.2, 0, 0]} />
-              {/* <OrbitControls /> */}
-          </Canvas>
-        </Wrapper>
-      )
+  const [zoom, setZoom] = useState(false)
+  const [focus, setFocus] = useState({})
+  return(
+      <Wrapper>
+        <Canvas camera={{ position: [0, 10, 10], castShadow: true }}>
+            <primitive object={new THREE.AxesHelper(10)} />
+            <ambientLight />
+            <directionalLight color="white" position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+            <Plane />
+
+        </Canvas>
+      </Wrapper>
+    )
 }
 
 export default CanvasTest;
