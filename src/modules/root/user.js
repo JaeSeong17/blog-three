ed8c7d53d1c22141498e40f6305dcf685b13eab2
AnJaeSeong <dktmzh6@gmail.com>
2023-03-01 01:04:53 +0900

    FIX: logout module at canvas
    - canvas내 component 트리 계층 문제로 인해 로그아웃 버튼이 정상 작동x
    - canvas내 html이 아닌 3d공간에서 logout시 트리에서 하위 노드에 있는 store에 등록된 saga를 정상적으로 호출하지 못함
    - root store에 있는 user module에서 logout api를 호출할 수 있도록 root store에 saga 등록을 추가

--- src/modules/root/user.js
+++ src/modules/root/user.js
@@ -1,4 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAction } from '@reduxjs/toolkit';
+import { takeLatest, call } from 'redux-saga/effects';
+import * as authAPI from '../../lib/api/auth';
+
+const LOGOUT = 'user/logout';
+export const logout = createAction(LOGOUT);
+function* logoutSaga() {
+  try {
+    console.log('try logout');
+    yield call(authAPI.logout);
+    localStorage.removeItem('user');
+  } catch (e) {
+    console.log(e);
+  }
+}
+export function* userSaga() {
+  yield takeLatest(LOGOUT, logoutSaga);
+}
 
 const user = createSlice({
   name: 'user',
