5095542fad91bbdb3170fcfa94a320cb34eb5e09
AnJaeSeong <dktmzh6@gmail.com>
2023-03-10 21:40:34 +0900

    TS-Migrate: ts migrate component(html/pages)

--- src/components/html/pages/PostPage.tsx
+++ src/components/html/pages/PostPage.tsx
@@ -1,7 +1,7 @@
 import HtmlWrapper from '../common/HtmlWrapper';
 import PostViewerContainer from '../post/PostViewerContainer';
 
-const PostPage = ({ currPostId }) => {
+const PostPage = ({ currPostId }: { currPostId: string }) => {
   return (
     <HtmlWrapper>
       <PostViewerContainer currPostId={currPostId} />
--- src/components/html/pages/WritePage.tsx
+++ src/components/html/pages/WritePage.tsx
@@ -1,9 +1,14 @@
+import { CurrPostParams } from 'root-state-types';
 import HtmlWrapper from '../common/HtmlWrapper';
 import EditorContainer from '../write/EditorContainer';
 import TagBoxContainer from '../write/TagBoxContainer';
 import WriteActionButtonsContainer from '../write/WriteActionButtonContainer';
 
-const WritePage = ({ writeComplete }) => {
+const WritePage = ({
+  writeComplete,
+}: {
+  writeComplete: (parms: CurrPostParams) => void;
+}) => {
   return (
     <HtmlWrapper>
       <EditorContainer />
