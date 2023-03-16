df03868963ec4fa960fb9e473e53132ade14dd82
AnJaeSeong <dktmzh6@gmail.com>
2023-03-17 00:35:17 +0900

    REFACTOR: sepearte tags set from keycap
    - 기존 Keycap에 있던 태그 리스트를 별도 파일로 분리

---
+++ src/static/tags.ts
@@ -0,0 +1,24 @@
+const tags = [
+  {
+    index: 1,
+    title: 'Total',
+  },
+  {
+    index: 2,
+    title: 'React',
+  },
+  {
+    index: 3,
+    title: 'JavaScript',
+  },
+  {
+    index: 4,
+    title: 'CS',
+  },
+  {
+    index: 5,
+    title: 'Dev Log',
+  },
+];
+
+export default tags;
