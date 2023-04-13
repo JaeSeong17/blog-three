8076922507937c463a5d055af8280678bf159c68
AnJaeSeong <dktmzh6@gmail.com>
2023-04-13 15:50:59 +0900

    REFACTOR: add pause option of tower animation

--- src/components/three/common/DataTower.tsx
+++ src/components/three/common/DataTower.tsx
@@ -1,15 +1,32 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Mesh } from 'three';
 import { dataTowerIdleAnim } from '../anim/SubObjectAnim';
+import { useSelector } from 'react-redux';
+import { RootState } from 'root-state-types';
 
 const DataTower = () => {
+  const target = useSelector((state: RootState) => state.camController.target);
   const ref = useRef<Array<Mesh | null>>([]);
+  const [idle, setIdle] = useState<gsap.core.Timeline>();
+  const onTarget = ['start', 'login', 'register'];
 
+  // 애니메이션 함수 변수에 할당
   useEffect(() => {
     if (ref.current && ref.current.every(item => item !== null)) {
-      dataTowerIdleAnim(ref.current as Array<Mesh>).play();
+      setIdle(dataTowerIdleAnim(ref.current as Array<Mesh>));
     }
   }, [ref]);
+
+  // play, pause
+  useEffect(() => {
+    if (idle) {
+      if (onTarget.includes(target)) {
+        idle.play();
+      } else {
+        idle.pause();
+      }
+    }
+  }, [idle, target]);
 
   return (
     <group>
